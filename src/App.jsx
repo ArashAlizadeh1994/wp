80e7b55bc78e5c83342ef2a22ffbba0d663ccbf6
ArashAlizadeh <alizadeh.nov@gmail.com>
2024-10-22 18:39:23 +0330

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Buttom from './components/buttom/Button'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Buttom />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/buttom/Button.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Buttom = () => {
+  return (
+    <div className='w-1/2 flex items-center justify-center mx-auto'>
+      <div className='bg-blue-400 p-px rounded-lg  from-blue-300 bg-gradient-to-b to-blue-800 border-blue-800 border'>
+          <button className='bg-blue-800 px-12 w-full text-white rounded-[7px] hover:bg-blue-700 transition duration-400 font-medium' >
+           CLICK
+          </button>
+      </div>
+    </div>
+  )
+}
+
+export default Buttom
